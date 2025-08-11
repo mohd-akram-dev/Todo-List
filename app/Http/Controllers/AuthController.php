edf17689024c750879582f4e2d6f0d62d2eff0c3
mdakku143 <mohdakram.akku143@gmail.com>
2025-08-11 10:51:17 +0400

    Sanctum implementation

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => Hash::make($fields['password'])
+        ]);
+
+        $token = $user->createToken('apptoken')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('apptoken')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out'], 200);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,4 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('todos', TodoController::class);
+
+Route::middleware("auth:sanctum")->group(function () {
+
+    Route::apiResource('todos', TodoController::class);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
+});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
